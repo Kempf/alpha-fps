6d08e7a1911b8198ea021bf59f3d58655db532fc
JimmyJK96 <jimmyjkenyon@gmail.com>
2014-07-22 19:23:44 +1000

    Added textures and bullet prefab

--- Assets/Scripts/Actions.cs
+++ Assets/Scripts/Actions.cs
@@ -6,13 +6,6 @@ public class Actions : MonoBehaviour {
 	public Transform BarrelEnd;
 	public Rigidbody Bullet;
 
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
 	void FixedUpdate () {
 	
 			if(Input.GetButtonDown("Fire1"))
