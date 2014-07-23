0920b3308d38d62be38572af412fa467330668f5
Paul Apelt <lt.kempf@gmail.com>
2014-07-23 15:35:06 +1000

    Fixed formatting

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -1,8 +1,6 @@
 using UnityEngine;
 using System.Collections;
 
-// TODO: Fix the fucking indents
-
 public class CharacterControl : MonoBehaviour {
 
 	//Look controls
@@ -24,15 +22,13 @@ public class CharacterControl : MonoBehaviour {
 	public int PKJump = 5000;
 
 	void Awake () {
-
 		Screen.lockCursor = true;
-
-		}
+	}
 	
 	
 	// player control function
 	
-	void InputMovement () {
+	private void InputMovement () {
 	  
 	  if (Input.GetKey ("escape"))
 			Screen.lockCursor = true; 
@@ -47,79 +43,75 @@ public class CharacterControl : MonoBehaviour {
 				RotZ -= 5;
 			}
 		} else if (RotZ > 0) {
-				RotZ -= 5;
+			  RotZ -= 5;
 		} else if (RotZ < 0) {
-						RotZ += 5;
+			  RotZ += 5;
 		}
 
-				//Raycasts for testing
-				Debug.DrawRay (transform.position, Vector3.down, Color.red);
-				Debug.DrawRay (transform.position, transform.TransformDirection (0, -1, 0), Color.red);
+		//Raycasts for testing
+		Debug.DrawRay (transform.position, Vector3.down, Color.red);
+		Debug.DrawRay (transform.position, transform.TransformDirection (0, -1, 0), Color.red);
 
-				//X Rotation
-				RotX += Input.GetAxis ("Mouse X") * sensitivityX;
-				transform.localEulerAngles = new Vector3 (0, RotX, transform.localEulerAngles.x);
+		//X Rotation
+		RotX += Input.GetAxis ("Mouse X") * sensitivityX;
+		transform.localEulerAngles = new Vector3 (0, RotX, transform.localEulerAngles.x);
 
-				//Y Rotation
-				RotY += Input.GetAxis ("Mouse Y") * sensitivityY;
-				RotY = Mathf.Clamp (RotY, MinY, MaxY);
-				transform.localEulerAngles = new Vector3 (-RotY, transform.localEulerAngles.y, RotZ);
+		//Y Rotation
+		RotY += Input.GetAxis ("Mouse Y") * sensitivityY;
+		RotY = Mathf.Clamp (RotY, MinY, MaxY);
+		transform.localEulerAngles = new Vector3 (-RotY, transform.localEulerAngles.y, RotZ);
 
 
-				//- Movement Controls ----------------------------------//
-				//Check onGround state
-				if (onGround != 3) {
-						if (Physics.Raycast (transform.position, Vector3.down, 1.5f)) 
-								onGround = 1;
-						else if (Physics.Raycast (transform.position, transform.TransformDirection (0, -1, 0), 1.5f))
-								onGround = 2;
-						else
-								onGround = 0;
-				}
+		//- Movement Controls ----------------------------------//
+		//Check onGround state
+		if (onGround != 3) {
+			if (Physics.Raycast (transform.position, Vector3.down, 1.5f)) 
+				onGround = 1;
+			else if (Physics.Raycast (transform.position, transform.TransformDirection (0, -1, 0), 1.5f))
+				onGround = 2;
+			else
+				onGround = 0;
+		}
 
-				//Movement while on the ground (onGround == 1)
-				if (onGround == 1) {
-						// Calculate how fast we should be moving
-						Vector3 targetVelocity = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
-						targetVelocity = transform.TransformDirection (targetVelocity);
-						targetVelocity *= speed;
+		//Movement while on the ground (onGround == 1)
+		if (onGround == 1) {
+		  // Calculate how fast we should be moving
+			Vector3 targetVelocity = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			targetVelocity = transform.TransformDirection (targetVelocity);
+			targetVelocity *= speed;
 			
-						// Apply a force that attempts to reach our target velocity
-						Vector3 velocity = rigidbody.velocity;
-						Vector3 velocityChange = (targetVelocity - velocity);
-						velocityChange.x = Mathf.Clamp (velocityChange.x, -maxVelocityChange, maxVelocityChange);
-						velocityChange.z = Mathf.Clamp (velocityChange.z, -maxVelocityChange, maxVelocityChange);
-						velocityChange.y = 0;
-						rigidbody.AddForce (velocityChange, ForceMode.VelocityChange);
-				}
+			// Apply a force that attempts to reach our target velocity
+			Vector3 velocity = rigidbody.velocity;
+			Vector3 velocityChange = (targetVelocity - velocity);
+			velocityChange.x = Mathf.Clamp (velocityChange.x, -maxVelocityChange, maxVelocityChange);
+			velocityChange.z = Mathf.Clamp (velocityChange.z, -maxVelocityChange, maxVelocityChange);
+			velocityChange.y = 0;
+			rigidbody.AddForce (velocityChange, ForceMode.VelocityChange);
+		}
 				
-				//General non flying scripts
-				if (onGround > 0) {
-						//Crouching onGround state
-						if (Input.GetButton ("Crouch"))
-								onGround = 3;
-						else
-								onGround = 0;
-
-						//If onGround is 1 or 2
-						if (onGround != 3) {
-								//Jumping
-								if (Input.GetButton ("Jump")) {
-										rigidbody.AddRelativeForce (0, PKJump, PKJump/10);
-								}
-						}
+		//General non flying scripts
+		if (onGround > 0) {
+			//Crouching onGround state
+			if (Input.GetButton ("Crouch"))
+				onGround = 3;
+			else
+				onGround = 0;
+			//If onGround is 1 or 2
+			if (onGround != 3) {
+				//Jumping
+				if (Input.GetButton ("Jump")) {
+					rigidbody.AddRelativeForce (0, PKJump, PKJump/10);
 				}
+			}
+		}
 		//Gravity
 		rigidbody.AddForce (Vector3.down * gravity * 100);
 
 	}
 	
 	void FixedUpdate () {
-	
-	// only fuck with the player if it belongs to you, okay?  
-	
-    if (networkView.isMine)
-    {
+	  // only fuck with the player if it belongs to you, okay?  
+    if (networkView.isMine) {
         InputMovement();
     }  
 	}
