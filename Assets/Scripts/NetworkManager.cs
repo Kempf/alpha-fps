e5bac3f070a46cd5919f94211195839a65232986
Paul Apelt <lt.kempf@gmail.com>
2014-07-23 15:22:40 +1000

    Network functions from "Menu"
    Moved the server connect and host functions over from the menu script and modified them

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -4,19 +4,30 @@ using System.Collections;
 public class NetworkManager : MonoBehaviour {
 
   public GameObject playerPrefab;
-   
-  void OnServerInitialized()
-  {
+  
+  // event handlers
+  void OnServerInitialized() {
       SpawnPlayer();
   }
    
-  void OnConnectedToServer()
-  {
+  void OnConnectedToServer() {
       SpawnPlayer();
   }
-   
-  private void SpawnPlayer()
-  {
+  
+  // Spawn player on the network from the prefab
+  private void SpawnPlayer() {
       Network.Instantiate(playerPrefab, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
   }
+  
+  // Yup, you guessed it. Starts the server. RLY.
+  public void StartServer() {
+		Network.InitializeServer (32, 25565, false); // connection number, port, use NAT
+		Network.sendRate = 15; // tickrate
+		Debug.Log ("Server Started");
+	}
+	
+	// do I really have to explain myself here?
+	public void ConnectToServer (string IPAddress) {
+		Network.Connect(IPAddress, 25565);
+	}
 }
